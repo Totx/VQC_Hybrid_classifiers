28b4a798ad606dca4b0865b2baac4291a7681181
Unai Lizarralde <totxivica@gmail.com>
2022-09-10 00:49:16 +0200

    Initial commit

---
+++ srcCode/srcCode.py
@@ -0,0 +1,10 @@
+# PennyLane libraries for quantum machine learning.
+import pennylane as qml
+import numpy as np
+from pennylane.optimize import AdamOptimizer, GradientDescentOptimizer
+
+# Standard computer science and data management libraries.
+import itertools as it
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sn
